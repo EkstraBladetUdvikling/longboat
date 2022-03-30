c331fddb91b01d7af2802340f5dcdd4763c9c7a0
tegner <jesper.h.tegner@jppol.dk>
2022-03-30 08:42:27 +0200

    linting

--- src/types.ts
+++ src/types.ts
@@ -34,32 +34,19 @@ export interface ILongboatVariables {
   st: string;
   url: string;
 }
-declare type ISiteProperties = Pick<
-  ILongboatVariables,
-  'bar' | 'bfcache' | 'cid' | 'nt' | 'skid' | 'st' | 'url'
->;
+declare type ISiteProperties = Pick<ILongboatVariables, 'bar' | 'bfcache' | 'cid' | 'nt' | 'skid' | 'st' | 'url'>;
 declare type IEventProperties = Pick<ILongboatVariables, 'ets' | 'ht'>;
-declare type ICoreProperties = Pick<
-  ILongboatVariables,
-  'aid' | 'cmpid' | 'csstatus' | 'ebid' | 'evid'
->;
+declare type ICoreProperties = Pick<ILongboatVariables, 'aid' | 'cmpid' | 'csstatus' | 'ebid' | 'evid'>;
 declare type IUserProperties = Pick<ILongboatVariables, 'kh' | 'lis' | 'ssoid'>;
-export declare type IMandatoryProps = ISiteProperties &
-  IEventProperties &
-  ICoreProperties &
-  IUserProperties;
+export declare type IMandatoryProps = ISiteProperties & IEventProperties & ICoreProperties & IUserProperties;
 /**
  * Event types
  */
 export declare type IPageviewData = IMandatoryProps &
-  Pick<
-    ILongboatVariables,
-    'at' | 'ekid' | 'ikid' | 'ipad' | 'la' | 'pfs' | 'ref'
-  >;
+  Pick<ILongboatVariables, 'at' | 'ekid' | 'ikid' | 'ipad' | 'la' | 'pfs' | 'ref'>;
 export declare type IContentinviewData = IMandatoryProps &
   Pick<ILongboatVariables, 'cids' | 'cmsaid' | 'ln' | 'recom_src'>;
-export declare type IScrollEventData = IMandatoryProps &
-  Pick<ILongboatVariables, 'scroll'>;
+export declare type IScrollEventData = IMandatoryProps & Pick<ILongboatVariables, 'scroll'>;
 export interface IVideoSpecificProps {
   vet: string;
   vidpid: string;
