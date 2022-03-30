4f8d2566201f33a288f3eb509818ee651c160744
tegner <jesper.h.tegner@jppol.dk>
2022-03-30 08:35:16 +0200

    linting

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import ts from 'rollup-plugin-ts';
 
 const sharedTerserOptions = {
   format: {
@@ -24,7 +24,7 @@ const terserOptions = {
 };
 
 const plugins = [
-  typescript(),
+  ts(),
   resolve({
     browser: true,
   }),
