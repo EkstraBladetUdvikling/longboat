d547e458372ea9412ac97acc7a8d8d92b18d24e6
tegner <jesper.h.tegner@jppol.dk>
2022-06-15 11:13:39 +0200

    added prepublish

---
+++ scripts/prepublish.mjs
@@ -0,0 +1,23 @@
+import { createInterface } from 'readline';
+import { exit, stdin, stdout } from 'process';
+
+import { createTypes } from './createtypes.mjs';
+
+const rl = createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.question(`Is longboat types up to date? [y/n] `, async (answer) => {
+  console.log('answer', answer);
+  switch (answer.toLowerCase()) {
+    case 'y':
+    case 'yes':
+      exit(0);
+    case 'n':
+    case 'no':
+    default:
+      await createTypes();
+      exit(0);
+  }
+});
